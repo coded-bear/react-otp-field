9de7176308cbbccc55a7d4ba3d716400b890b975
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2021-10-26 23:44:51 +0200

    added demo, webpack

---
+++ src/demo/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import OtpInput from '../../lib';
+
+const Demo = () => <OtpInput />;
+
+render(<Demo />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/demo'),
+  output: {
+    path: path.join(__dirname, 'demo'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/demo/index.html'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'demo'),
+    port: 8080,
+    compress: true,
+  },
+};
